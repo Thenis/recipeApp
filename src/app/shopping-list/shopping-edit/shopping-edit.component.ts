7ef1d0dac70abaa0ad8337d199dcb3dfcb45d9a6
Thenis <d.stoilov96@gmail.com>
2017-07-20 15:13:10 +0300

    Added dropdown directive for dropdowns

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter, ElementRef } from '@angular/core';
+import { Ingredient } from './../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() onIngredientAdd = new EventEmitter<Ingredient>();
+
+  @ViewChild('nameInput') name: ElementRef;
+  @ViewChild('amountInput') amount: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddIngredient(e) {
+    e.preventDefault();
+    const ingredient = new Ingredient(this.name.nativeElement.value, this.amount.nativeElement.value)
+    this.onIngredientAdd.emit(ingredient);
   }
 
 }
